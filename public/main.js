4b5dd67e2e9c8025213d63174bc04c60e384977f
ChoiJuyoung <cjy020718@gmail.com>
2022-06-07 13:31:44 +0900

    Feat: port연결 및 title 만 화면에 나타내기

--- public/main.js
+++ public/main.js
@@ -1,5 +1,27 @@
 $(document).ready(function() {
   $("button").click(function(){
-    $("#demo").text(Date());
+    event.preventDefault();
+    display();
   })
 })
+
+function display() {
+  var xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
+  
+  xhr.addEventListener("readystatechange", function() {
+    if(this.readyState === 4) {
+      console.log(this.responseText);
+      var parsed = JSON.parse(this.responseText) 
+      for(i in parsed.documents) {
+        var cur = parsed.documents[i].title;
+        var pid = "demo" + i;
+        document.getElementById(pid).innerHTML += cur;
+      }
+    }
+  });
+  
+  xhr.open("GET", "http://localhost:3000/webapi?word="+encodeURI('용기'));
+  xhr.setRequestHeader("Authorization", "KakaoAK 62ecc8a4a163e78b24348694a6d18cf9");
+  xhr.send();
+}
